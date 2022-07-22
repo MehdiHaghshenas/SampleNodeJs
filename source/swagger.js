1f520d9c36f5cf71e9dd321b269e197e1fa109f6
mehdi <mhaghshenas@gmail.com>
2022-07-23 01:07:46 +0430

    add to do and go to bed

--- source/swagger.js
+++ source/swagger.js
@@ -1,6 +1,7 @@
 const swaggerAutogen = require("swagger-autogen")()
 
 const outputFile = "./source/swagger_output.json"
+//TODO get all ts files in routes
 const endpointsFiles = ["./source/routes/sample.routing.ts"]
 
 swaggerAutogen(outputFile, endpointsFiles)
