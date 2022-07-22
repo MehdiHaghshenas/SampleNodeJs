e804ae7354efe3e5c5b26c26fefb1bba1d8775d1
mehdi <mhaghshenas@gmail.com>
2022-07-23 00:18:14 +0430

    add swagger

--- source/server.ts
+++ source/server.ts
@@ -1,43 +1,73 @@
-import http from 'http';
-import express, { Express } from 'express';
-import morgan from 'morgan';
-import sampleRouter from './routes/sample.routing';
+import http from "http"
+import express, { Express } from "express"
+import morgan from "morgan"
+import sampleRouter from "./routes/sample.routing"
+import swaggerUi from "swagger-ui-express"
 
-const router: Express = express();
+import fs from "fs"
 
-/** Logging */
-router.use(morgan('dev'));
-/** Parse the request */
-router.use(express.urlencoded({ extended: false }));
-/** Takes care of JSON data */
-router.use(express.json());
+const outputFile = "./swagger_output.json"
 
-/** RULES OF OUR API */
-router.use((req, res, next) => {
+try {
+  if (!fs.existsSync(outputFile)) {
+    fs.writeFile(outputFile, "{}", function (err) {
+      if (err) throw err
+      console.log("File is created successfully.")
+    })
+  }
+} catch (err) {
+  console.error(err)
+}
+
+const swaggerAutogen = require("swagger-autogen")()
+
+const endpointsFiles = ["./routes/sample.routing.js"]
+
+swaggerAutogen(outputFile, endpointsFiles).then(() => {
+  const swaggerFile = require("./swagger_output.json")
+  const router: Express = express()
+
+  /** Logging */
+  router.use(morgan("dev"))
+  /** Parse the request */
+  router.use(express.urlencoded({ extended: false }))
+  /** Takes care of JSON data */
+  router.use(express.json())
+
+  /** RULES OF OUR API */
+  router.use((req, res, next) => {
     // set the CORS policy
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*")
     // set the CORS headers
-    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+    res.header(
+      "Access-Control-Allow-Headers",
+      "origin, X-Requested-With,Content-Type,Accept, Authorization"
+    )
     // set the CORS method headers
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
-        return res.status(200).json({});
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST")
+      return res.status(200).json({})
     }
-    next();
-});
+    next()
+  })
+  /* agg swagger */
+  router.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-/** Routes */
-router.use('/', sampleRouter);
+  /** Routes */
+  router.use("/", sampleRouter)
 
-/** Error handling */
-router.use((req, res, next) => {
-    const error = new Error('not found');
+  /** Error handling */
+  router.use((req, res, next) => {
+    const error = new Error("not found")
     return res.status(404).json({
-        message: error.message
-    });
-});
+      message: error.message,
+    })
+  })
 
-/** Server */
-const httpServer = http.createServer(router);
-const PORT: any = process.env.PORT ?? 6060;
-httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+  /** Server */
+  const httpServer = http.createServer(router)
+  const PORT: any = process.env.PORT ?? 6060
+  httpServer.listen(PORT, () =>
+    console.log(`The server is running on port ${PORT}`)
+  )
+})
