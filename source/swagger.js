97ca395f0634963f4c37617fd5e5185245afc958
Mehdi Haghshenas <mhaghshenas@gmail.com>
2022-07-23 19:47:43 +0430

    Fix Swagger automaticly search all ts in rourte folder

--- source/swagger.js
+++ source/swagger.js
@@ -1,7 +1,27 @@
-const swaggerAutogen = require("swagger-autogen")()
+swaggerAutogen =require('swagger-autogen')()
 
 const outputFile = "./source/swagger_output.json"
+
 //TODO get all ts files in routes
-const endpointsFiles = ["./source/routes/sample.routing.ts"]
+const path = require("path");
+const fs   = require("fs");
+
+function throughDirectory(Directory) {
+    fs.readdirSync(Directory).forEach(file => {
+        const filename = path.join(Directory, file);
+        const stat = fs.statSync(filename)
+        if (stat.isDirectory()) return throughDirectory(filename);
+        else if (path.extname(file).toLowerCase() == '.ts')
+            return endpointsFiles.push(filename);
+    });
+}
+
+// const endpointsFiles = ["./source/routes/sample.routing.ts"]
+const endpointsFiles = []
+
+throughDirectory("./source/routes/");
+
+console.log(endpointsFiles)
+
 
 swaggerAutogen(outputFile, endpointsFiles)
